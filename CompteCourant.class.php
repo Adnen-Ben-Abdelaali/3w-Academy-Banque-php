7e633792f74e71ef95c1a913ac72462f28652550
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2020-02-28 09:47:16 +0100

    classe 'CompteEpargne'finalisée

--- CompteCourant.class.php
+++ CompteCourant.class.php
@@ -1,5 +1,26 @@
 <?php
   
+  class CompteCourant extends CompteBancaire 
+  {
+
+    public function retirerArgent(float $mont) {
+
+      if( ($mont > 0) && ($mont < $this->solde) ) 
+      {
+
+        echo "Vous avez retiré : " .$mont ." TND \n" ;
+
+        $this->solde -= $mont;
+
+        array_push($this->tracesCompte, array("-", date('r'), $mont, $this->solde));
+      
+      }else 
+      {
+        echo "ERROR : try again later";
+      }
+    }
+
+  }
 
 
 
